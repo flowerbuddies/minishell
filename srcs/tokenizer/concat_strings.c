5ac9be9d3fd6970c49154984ce39a30dd648601b
Mark-Felix Müller <marmulle@student.hive.fi>
2023-06-23 18:12:40 +0300

    fix(tokenizer): refactor while loops, fix segv, handle malloc fail (#18)

--- srcs/tokenizer/concat_strings.c
+++ srcs/tokenizer/concat_strings.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   concat_strings.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hunam <hunam@student.42.fr>                +#+  +:+       +#+        */
+/*   By: marmulle <marmulle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 20:19:33 by hunam             #+#    #+#             */
-/*   Updated: 2023/06/22 16:03:49 by hunam            ###   ########.fr       */
+/*   Updated: 2023/06/23 18:02:59 by marmulle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,23 +14,22 @@
 #include "tokenizer.h"
 #include <stdlib.h>
 
-static int	get_total_len(t_token *first_string_token)
+static int	get_total_len(t_token *token)
 {
-	t_token	*current;
 	int		total_len;
 
-	current = first_string_token;
-	total_len = 1;
-	while (current->next && current->next->type == STRING)
+	total_len = 0;
+	while (token->type == STRING)
 	{
-		total_len += ft_strlen(current->data);
-		current = current->next;
+		total_len += ft_strlen(token->data);
+		token = token->next;
+		if (!token)
+			break ;
 	}
-	total_len += ft_strlen(current->data);
 	return (total_len);
 }
 
-static char	*get_total_str(t_token *first_string_token)
+static char	*get_total_str(t_tokenizer *tokenizer, t_token *first_string_token)
 {
 	t_token		*current;
 	int			current_str_i;
@@ -39,40 +38,51 @@ static char	*get_total_str(t_token *first_string_token)
 	const int	total_len = get_total_len(first_string_token);
 
 	current = first_string_token;
-	total_str = malloc(sizeof(char) * total_len);
-	total_str_i = 0;
+	total_str = malloc(sizeof(char) * (total_len + 1));
 	if (!total_str)
+	{
+		tokenizer->errored = true;
 		return (NULL);
-	while (current->next && current->next->type == STRING)
+	}
+	total_str_i = 0;
+	while (current->type == STRING)
 	{
 		current_str_i = -1;
 		while (current->data[++current_str_i])
 			total_str[total_str_i++] = current->data[current_str_i];
 		current = current->next;
+		if (!current)
+			break ;
 	}
-	current_str_i = -1;
-	while (current->data[++current_str_i])
-		total_str[total_str_i++] = current->data[current_str_i];
 	total_str[total_str_i] = '\0';
 	return (total_str);
 }
 
-static void	free_remaining_tokens(t_token *tokens)
+static void	free_remaining_string_tokens(t_token *tokens)
 {
-	if (tokens->next && tokens->next->type == STRING)
-		free_remaining_tokens(tokens->next);
-	free(tokens->data);
-	free(tokens);
+	t_token	*current;
+
+	current = tokens;
+	while (current->type == STRING)
+	{
+		free(current->data);
+		tokens = current->next;
+		free(current);
+		current = tokens;
+		if (!current)
+			break ;
+	}
 }
 
-void	concat_string_tokens(t_token *tokens)
+void	concat_string_tokens(t_tokenizer *tokenizer)
 {
+	t_token	*tokens;
 	t_token	*first_string_token;
 	t_token	*current;
-	t_token	*tmp;
 	char	*total_str;
 
-	while (tokens->next)
+	tokens = tokenizer->tokens;
+	while (tokens)
 	{
 		if (tokens->type == STRING)
 		{
@@ -82,12 +92,11 @@ void	concat_string_tokens(t_token *tokens)
 				current = current->next;
 			if (first_string_token != current)
 			{
-				total_str = get_total_str(first_string_token);
+				total_str = get_total_str(tokenizer, first_string_token);
 				free(first_string_token->data);
 				first_string_token->data = total_str;
-				tmp = current->next;
-				free_remaining_tokens(first_string_token->next);
-				first_string_token->next = tmp;
+				free_remaining_string_tokens(first_string_token->next);
+				first_string_token->next = current->next;
 			}
 		}
 		tokens = tokens->next;
