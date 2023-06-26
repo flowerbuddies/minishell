a09544624a5fa75789643670281a3755f7a6a85a
Hunam <hunam@disroot.org>
2023-06-26 15:41:49 +0300

    string

--- tests/test.h
+++ tests/test.h
@@ -6,7 +6,7 @@
 /*   By: hunam <hunam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 19:14:11 by hunam             #+#    #+#             */
-/*   Updated: 2023/06/26 15:27:56 by hunam            ###   ########.fr       */
+/*   Updated: 2023/06/26 15:34:39 by hunam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,33 @@
 # include "munit/munit.h"
 
 MunitResult	test_demo(const MunitParameter[], void *);
+
 MunitResult	test_raw_string_1(const MunitParameter[], void *);
 MunitResult	test_raw_string_2(const MunitParameter[], void *);
 MunitResult	test_raw_string_3(const MunitParameter[], void *);
 MunitResult	test_raw_string_4(const MunitParameter[], void *);
 
+MunitResult	test_string_1(const MunitParameter[], void *);
+MunitResult	test_string_2(const MunitParameter[], void *);
+MunitResult	test_string_3(const MunitParameter[], void *);
+MunitResult	test_string_4(const MunitParameter[], void *);
+MunitResult	test_string_5(const MunitParameter[], void *);
+
 const MunitSuite	tokenizer_suite = {
 	"/tokenizer",
 	(MunitTest[]){
 		{"/test_demo", test_demo, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+		
 		{"/test_raw_string_1", test_raw_string_1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 		{"/test_raw_string_2", test_raw_string_2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 		{"/test_raw_string_3", test_raw_string_3, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 		{"/test_raw_string_4", test_raw_string_4, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+
+		{"/test_string_1", test_string_1, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+		{"/test_string_2", test_string_2, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+		{"/test_string_3", test_string_3, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+		{"/test_string_4", test_string_4, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
+		{"/test_string_5", test_string_5, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL},
 		{NULL, NULL, NULL, NULL, MUNIT_TEST_OPTION_NONE, NULL}
 	}, NULL, 1, MUNIT_SUITE_OPTION_NONE
 };
--- tests/test_tokenizer.c
+++ tests/test_tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: hunam <hunam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 18:10:46 by hunam             #+#    #+#             */
-/*   Updated: 2023/06/26 15:24:48 by hunam            ###   ########.fr       */
+/*   Updated: 2023/06/26 15:33:44 by hunam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -93,5 +93,65 @@ MunitResult	test_raw_string_4(const MunitParameter params[], void *user_data_or_
 		(t_type []){PIPE, SPACE, STRING, REDIR_OUT, STRING},
 		(char *[]){NULL, NULL,   "abc", NULL,       "def"},
 		5);
+	return (MUNIT_OK);
+}
+
+MunitResult	test_string_1(const MunitParameter params[], void *user_data_or_fixture)
+{
+	(void) params;
+	(void) user_data_or_fixture;
+	tokenizer_one_test(
+		"\"a\"",
+		(t_type []){STRING},
+		(char *[]){"a"},
+		1);
+	return (MUNIT_OK);
+}
+
+MunitResult	test_string_2(const MunitParameter params[], void *user_data_or_fixture)
+{
+	(void) params;
+	(void) user_data_or_fixture;
+	tokenizer_one_test(
+		"\"abc\"",
+		(t_type []){STRING},
+		(char *[]){"abc"},
+		1);
+	return (MUNIT_OK);
+}
+
+MunitResult	test_string_3(const MunitParameter params[], void *user_data_or_fixture)
+{
+	(void) params;
+	(void) user_data_or_fixture;
+	tokenizer_one_test(
+		"\"abc\"\"def\"",
+		(t_type []){STRING},
+		(char *[]){"abcdef"},
+		1);
+	return (MUNIT_OK);
+}
+
+MunitResult	test_string_4(const MunitParameter params[], void *user_data_or_fixture)
+{
+	(void) params;
+	(void) user_data_or_fixture;
+	tokenizer_one_test(
+		"| \"abc\">\"def\"",
+		(t_type []){PIPE, SPACE, STRING, REDIR_OUT, STRING},
+		(char *[]){NULL, NULL,   "abc", NULL,       "def"},
+		5);
+	return (MUNIT_OK);
+}
+
+MunitResult	test_string_5(const MunitParameter params[], void *user_data_or_fixture)
+{
+	(void) params;
+	(void) user_data_or_fixture;
+	tokenizer_one_test(
+		"\"abc\"'def'>",
+		(t_type []){STRING, REDIR_OUT},
+		(char *[]){"abcdef", NULL},
+		2);
 	return (MUNIT_OK);
 }
