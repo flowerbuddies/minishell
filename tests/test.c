dc4b311f5338afa43c4d03c1c024ade016fb911a
Hunam <hunam@disroot.org>
2023-06-29 23:26:59 +0300

    fix(env_var): fix #45, fix #46, add tests

--- tests/test.c
+++ tests/test.c
@@ -6,15 +6,18 @@
 /*   By: hunam <hunam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/23 16:31:53 by hunam             #+#    #+#             */
-/*   Updated: 2023/06/23 19:19:48 by hunam            ###   ########.fr       */
+/*   Updated: 2023/06/29 23:19:01 by hunam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #define MUNIT_ENABLE_ASSERT_ALIASES
 #include "munit/munit.h"
 #include "test.h"
+#include "../srcs/minishell.h"
+#include "../srcs/env_var/env_var.h"
 
 int	main(int ac, char *av[])
 {
+	vars_append(&g_shell.vars, new_var("TEMP", "VALUE_WOW"));
 	return (munit_suite_main(&tokenizer_suite, NULL, ac, av));
 }
