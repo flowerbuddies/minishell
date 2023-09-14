dd6b2a705531dc656a3f0944bf8b626ecc5233e0
Hunam <hunam@disroot.org>
2023-09-14 20:04:05 +0300

    feat(builtins/export): support multiple args

--- srcs/builtin/export.c
+++ srcs/builtin/export.c
@@ -6,7 +6,7 @@
 /*   By: hunam <hunam@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/07 01:40:20 by hunam             #+#    #+#             */
-/*   Updated: 2023/09/14 19:14:36 by hunam            ###   ########.fr       */
+/*   Updated: 2023/09/14 20:03:22 by hunam            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,31 +42,35 @@ void	export(t_token *cmd, bool is_parent)
 			vars_print(true);
 		return ;
 	}
-	if (!ft_strchr(cmd->data, '='))
-		return ;
-	parts = ft_split(cmd->data, '=');
-	if (!parts)
-		return ((void)(g_shell.exit_status = failure));
-	parts_len = len2d(parts);
-	if (parts_len != 1 && parts_len != 2) //TODO(mark): fix export 5f
+	while (cmd)
 	{
-		g_shell.exit_status = failure;
-		return (free2d(parts));
-	}
-	if (!is_valid_identifier(parts[0]))
-	{
+		if (!ft_strchr(cmd->data, '='))
+			return ;
+		parts = ft_split(cmd->data, '=');
+		if (!parts)
+			return ((void)(g_shell.exit_status = failure));
+		parts_len = len2d(parts);
+		if (parts_len != 1 && parts_len != 2) //TODO(mark): fix export 5f
+		{
+			g_shell.exit_status = failure;
+			return (free2d(parts));
+		}
+		if (!is_valid_identifier(parts[0]))
+		{
+			if (!is_parent)
+				eprintf("\e[31;1mError:\e[0m invalid identifier\n", NULL, NULL);
+			g_shell.exit_status = failure;
+			return (free2d(parts));
+		}
 		if (!is_parent)
-			eprintf("\e[31;1mError:\e[0m invalid identifier\n", NULL, NULL);
-		g_shell.exit_status = failure;
-		return (free2d(parts));
+			return (free2d(parts));
+		if (vars_find(parts[0]))
+			vars_delete_at(parts[0]);
+		if (parts_len == 1)
+			vars_append(vars_new(parts[0], ft_strdup("")));
+		else
+			vars_append(vars_new(parts[0], parts[1]));
+		free(parts);
+		cmd = cmd->next;
 	}
-	if (!is_parent)
-		return (free2d(parts));
-	if (vars_find(parts[0]))
-		vars_delete_at(parts[0]);
-	if (parts_len == 1)
-		vars_append(vars_new(parts[0], ft_strdup("")));
-	else
-		vars_append(vars_new(parts[0], parts[1]));
-	free(parts);
 }
